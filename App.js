37935533b40d3a769fcc760e30448124bb4e2338
Venkatavamsi-123 <venkatavamsi448@gmail.com>
2024-12-04 00:09:00 +0530

    episode-3

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-const heading=React.createElement("h1",{},"Hello world");
-    const root=ReactDOM.createRoot(document.getElementById("root"));
-    root.render(heading)
+const jsxheading=(
+    <h1 className="head" tabIndex="5">
+        Namaste React using Jsx
+    </h1>
+);
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxheading)
